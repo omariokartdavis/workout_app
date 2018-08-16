47c9760abc532fcc35afcb9d93e56e88dda7613d
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2018-08-16 14:15:31 -0500

    Update workoutapp.py
    added login screen functionality

--- workoutapp.py
+++ workoutapp.py
@@ -1,22 +1,67 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
-class ScreenOne(Screen):
+class FailedLoginPopup(Popup):
     pass
 
-class ScreenTwo(Screen):
+class NewAccountPopup(Popup):
+    
+    def add_credentials(self):
+        LoginScreen.login_credentials[self.new_username.text] = self.new_password.text
+    
+class LoginScreen(Screen):
+    username = ObjectProperty()
+    password = ObjectProperty()
+    new_username: ObjectProperty()
+    new_password: ObjectProperty()
+    
+    login_credentials = {'omari' : 'davis', '' : ''}
+    
+    def check_credentials(self):
+        
+        if self.username.text in self.login_credentials and self.password.text == self.login_credentials[self.username.text]:
+            self.username.text = ""
+            self.password.text = ""
+            self.manager.transition.direction = "up"
+            self.manager.transition.duration = 1
+            self.manager.current = "macrocycle_screen"
+        else:
+            self.username.text = ""
+            self.password.text = ""
+            self.failed_login()
+        
+    def failed_login(self):
+        the_popup = FailedLoginPopup()
+        the_popup.open()
+
+    def new_account(self):
+        the_popup = NewAccountPopup()
+        the_popup.open()
+        
+class MainMenuScreen(Screen):
     pass
 
-class ScreenThree(Screen):
+class MacrocycleScreen(Screen):
+    pass
+
+class MesocycleScreen(Screen):
+    pass
+
+class MicrocycleScreen(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
     pass
 
-
 class WorkoutApp(App):
-    pass
+    def build(self):
+        Window.clearcolor = (1, 1, 1, 1)
+        return
 
 sample_app = WorkoutApp()
 sample_app.run()
